8b084088fd2c6e46529edf041c2c4f7c14e82023
Ryan Kuang <ryankuang@ymail.com>
2016-06-14 21:59:21 +0000

    made a quiz

--- client/js/base.js
+++ client/js/base.js
@@ -1,19 +1,42 @@
 function MyController($scope, $http) {
-    
-    $scope.people = [];
+
+    $scope.questions = [];
+    $scope.currentQuestion = null;
     $scope.color = '';
     $scope.message = '';
-    
+    $scope.done = false;
+    var questionIndex = 0;
+    var answers = [];
+
     $scope.postData = function() {
-        $http.post('/api/v1/postData', {color: $scope.color}).then(function(response) {
+        $http.post('/api/v1/postData', {
+            color: $scope.color
+        }).then(function(response) {
             $scope.message = response.data.message;
         });
         $scope.color = '';
     };
-    
+
     $scope.getData = function() {
         $http.get('/api/v1/getData').then(function(response) {
-            $scope.people = response.data;
+            $scope.questions = response.data;
+
+            $scope.currentQuestion = $scope.questions[0];
+
         })
+    };
+
+    $scope.answerQuestion = function(answerValue) {
+        console.log('answerQuestion: ' + answerValue);
+        answers.push(answerValue);
+        questionIndex = questionIndex + 1;
+        console.log('questionIndex: ' + questionIndex);
+        console.log('currentQuestion: ' + JSON.stringify($scope.questions[questionIndex]));
+        $scope.currentQuestion = $scope.questions[questionIndex];
+        if (questionIndex == $scope.questions.length) {
+            $scope.done = true;
+            console.log('You have finished!');
+            questionIndex = 0;
+        }
     };
 }
--- server.js
+++ server.js
@@ -25,12 +25,83 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 // API Routes
 router.get('/api/v1/getData', function(request, response) {
     var data = [
-        {name: 'Ryan', color: 'red'},
-        {name: 'Trenton', color: 'orange'},
-        {name: 'Rachel', color: 'yellow'},
-        {name: 'Nolan', color: 'green'},
-        {name: 'Trevor', color: 'blue'},
-        {name: 'Nathan', color: 'purple'}
+        {
+            question: '1. Which 72 - 10 record did the Warriors 2015 - 2016 break?', 
+            correctAnswer: 2,
+            answers: [
+                {id: 1, value: 'Bulls 1994 - 1995'},
+                {id: 2, value: 'Bulls 1995 - 1996'},
+                {id: 3, value: 'Bulls 1996 - 1997'},
+                {id: 4, value: 'Bulls 1997 - 1998'},
+                {id: 5, value: 'Bulls 1998 - 1999'}
+            ]
+        },
+        {
+            question: 'Who did not make it to the 50% fg - 40% 3pts - 90% ft club?', 
+            correctAnswer: 5,
+            answers: [
+                {id: 1, value: 'Stephen Curry 2015 - 2016'},
+                {id: 2, value: 'Steve Nash 2007 - 2008'},
+                {id: 3, value: 'Kevin Durant 2012 - 2013'},
+                {id: 4, value: 'Larry Bird 1986 - 1987'},
+                {id: 5, value: 'Lebron James 2011 - 2012'}
+            ]
+        },
+        {
+            question: 'How many points did Kobe Bryant score on his last game before he retired?', 
+            correctAnswer: 3,
+            answers: [
+                {id: 1, value: '50'},
+                {id: 2, value: '54'},
+                {id: 3, value: '60'},
+                {id: 4, value: '81'},
+                {id: 5, value: '61'}
+            ]
+        },
+        {
+            question: 'Who made the most points (100) in one nba game?', 
+            correctAnswer: 1,
+            answers: [
+                {id: 1, value: 'Wilt Chamberlain'},
+                {id: 2, value: 'Kobe Bryant'},
+                {id: 3, value: 'Kareem Abdul Jabbar'},
+                {id: 4, value: 'Stephen Curry'},
+                {id: 5, value: 'Michael Jordan'}
+            ]
+        },
+        {
+            question: 'Who made the most thress in a season and how many?', 
+            correctAnswer: 2,
+            answers: [
+                {id: 1, value: ''},
+                {id: 2, value: ''},
+                {id: 3, value: ''},
+                {id: 4, value: ''},
+                {id: 5, value: ''}
+            ]
+        },
+        {
+            question: '?', 
+            correctAnswer: 4,
+            answers: [
+                {id: 1, value: ''},
+                {id: 2, value: ''},
+                {id: 3, value: ''},
+                {id: 4, value: ''},
+                {id: 5, value: ''}
+            ]
+        },
+        {
+            question: '?', 
+            correctAnswer: 5,
+            answers: [
+                {id: 1, value: ''},
+                {id: 2, value: ''},
+                {id: 3, value: ''},
+                {id: 4, value: ''},
+                {id: 5, value: ''}
+            ]
+        }
     ];
     response.send(data);
 });
